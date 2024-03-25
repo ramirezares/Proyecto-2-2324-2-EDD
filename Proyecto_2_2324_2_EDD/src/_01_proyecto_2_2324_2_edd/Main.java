ea5a9f5a44f2f2793b86cfe1e4d692969d06515a
Daniela Zambrano <d.zambrano@correo.unimet.edu.ve>
2024-03-25 10:22:44 -0400

    quite algo

--- Proyecto_2_2324_2_EDD/src/_01_proyecto_2_2324_2_edd/Main.java
+++ Proyecto_2_2324_2_EDD/src/_01_proyecto_2_2324_2_edd/Main.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package _01_proyecto_2_2324_2_edd;
 
 import _06_Interfaces.Home;
